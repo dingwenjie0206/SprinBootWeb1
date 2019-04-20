a54e373a3dbb16ee855c919b0b74c01db182e4fa
dingwenjie <1336766272@qq.com>
2019-04-20 11:17:53 +0800

    first commit

---
+++ src/main/java/com/jxau/springbootweb/Controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.jxau.springbootweb.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+//    @RequestMapping({"/","/index.html"})
+////    public String index(){
+////        return "index";
+////    }
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+        map.put("hello","<h1>你好</h1>");
+        map.put("users", Arrays.asList("张三","李四","王五"));
+        return "success";
+    }
+
+}
