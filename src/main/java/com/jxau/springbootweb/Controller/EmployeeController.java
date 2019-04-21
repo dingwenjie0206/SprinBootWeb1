39bb61345c9ab29de280b8d88dabc2f1dc152e81
dingwenjie <1336766272@qq.com>
2019-04-21 11:15:14 +0800

    second commit

---
+++ src/main/java/com/jxau/springbootweb/Controller/EmployeeController.java
@@ -0,0 +1,44 @@
+package com.jxau.springbootweb.Controller;
+
+import com.jxau.springbootweb.dao.DepartmentDao;
+import com.jxau.springbootweb.dao.EmployeeDao;
+import com.jxau.springbootweb.entities.Department;
+import com.jxau.springbootweb.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @Autowired
+    DepartmentDao departmentDao;
+
+    @GetMapping("/emps")
+    public String list(Model model){
+         Collection<Employee> employees = employeeDao.getAll();
+         model.addAttribute("emps",employees);
+        return "emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+}
